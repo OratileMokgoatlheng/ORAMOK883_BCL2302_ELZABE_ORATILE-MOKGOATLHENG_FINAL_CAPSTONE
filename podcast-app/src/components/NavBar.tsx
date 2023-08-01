e8321244705da14ad8cd818b5603dc8428d66cf9
OratileMokgoatlheng <mokgoatlhengo@gmail.com>
2023-08-01 09:13:08 +0200

    Initial commit

---
+++ podcast-app/src/components/NavBar.tsx
@@ -0,0 +1,58 @@
+// NavigationBar.tsx
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+interface NavigationBarProps {
+  onSearch: (query: string) => void;
+  onSort: (ascending: boolean) => void;
+  onThemeChange: () => void;
+}
+
+function NavBar({ onSearch, onSort, onThemeChange }: NavigationBarProps) {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearchSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    onSearch(searchQuery);
+  };
+
+  const handleSortOptionChange = () => {
+    onSort(true); 
+  };
+
+  const handleThemeChange = () => {
+    onThemeChange();
+  };
+
+  return (
+    <div className="navigation-bar">
+      <Link to="/">
+        <button>Back</button>
+      </Link>
+      <form onSubmit={handleSearchSubmit} className="search-bar">
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={handleSearchInputChange}
+          placeholder="Search for a podcast..."
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div className="sort-option">
+        <label>
+          A-Z
+          <input type="radio" name="sort-option" onChange={handleSortOptionChange} />
+        </label>
+      </div>
+      <div className="theme-toggle">
+        <button onClick={handleThemeChange}>Toggle Theme</button>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
--- podcast-app/src/components/Previews.tsx
+++ podcast-app/src/components/Previews.tsx
@@ -1,7 +1,7 @@
 // Preview.tsx
-import { useState, useEffect } from 'react';
-
-const API_URL = 'https://podcast-api.netlify.app/shows'; 
+import { useState, useEffect } from "react";
+import NavBar from './NavBar'
+const API_URL = "https://podcast-api.netlify.app/shows";
 
 interface Podcast {
   image: string;
@@ -9,9 +9,9 @@ interface Podcast {
   description: string;
   seasons: number;
 }
-
 function Preview() {
   const [podcasts, setPodcasts] = useState<Podcast[]>([]);
+  const [selectedPodcast, setSelectedPodcast] = useState<Podcast | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,32 +21,62 @@ function Preview() {
           const data = await response.json();
           setPodcasts(data);
         } else {
-          throw new Error('Failed to fetch podcast data');
+          throw new Error("Failed to fetch podcast data");
         }
       } catch (error: any) {
-        console.error('Error fetching podcast data:', error.message);
+        console.error("Error fetching podcast data:", error.message);
       }
     };
 
     fetchData();
   }, []);
 
+  const handleSeeDescriptionClick = (podcast: Podcast) => {
+    setSelectedPodcast(podcast);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedPodcast(null);
+  };
+
   if (podcasts.length === 0) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
-      {podcasts.map((podcast, index) => (
-        <div key={index}>
-          <h1>{podcast.title}</h1>
-          <img src={podcast.image} alt={podcast.title} />
-          <p>{podcast.description}</p>
-          <p>Number of Seasons: {podcast.seasons}</p>
-          <hr /> 
-        </div>
-      ))}
-    </div>
+    <>
+      <NavBar />
+      <div className="card-container">
+        {podcasts.map((podcast, index) => (
+          <div className="card" key={index}>
+            <img
+              src={podcast.image}
+              alt={podcast.title}
+              className="card-image"
+            />
+            <div className="card-content">
+              <h1>{podcast.title}</h1>
+              {selectedPodcast === podcast ? (
+                <div className="description-modal">
+                  <div className="description-modal-content">
+                    <p>{podcast.description}</p>
+                    <button onClick={handleCloseModal}>Close</button>
+                  </div>
+                  <div
+                    className="description-modal-overlay"
+                    onClick={handleCloseModal}
+                  />
+                </div>
+              ) : (
+                <button onClick={() => handleSeeDescriptionClick(podcast)}>
+                  See Description
+                </button>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
