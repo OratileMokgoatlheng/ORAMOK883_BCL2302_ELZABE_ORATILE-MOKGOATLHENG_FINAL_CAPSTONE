5b6555d3339023d14e06c693127d3b7018ece91e
ORATILE  MOKGOATLHENG <125035665+OratileMokgoatlheng@users.noreply.github.com>
2024-02-29 23:21:18 +0200

    refined my code
    cleaning code to look much cleaner

--- podcast-app/src/components/Header.tsx
+++ podcast-app/src/components/Header.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import MicIcon from "@mui/icons-material/Mic";
-//import { Switch }  from "@mui/material"
 
 export const Header = () => {
 const navigate = useNavigate()
