62290223185c353301606431b2dbe0d0a009057a
ORATILE  MOKGOATLHENG <125035665+OratileMokgoatlheng@users.noreply.github.com>
2024-02-29 23:18:59 +0200

    refined my code

--- podcast-app/src/main.tsx
+++ podcast-app/src/main.tsx
@@ -1,15 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App.tsx";
-
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
@@ -17,15 +5,13 @@ import { FavoritesPage } from './components/FavoritesPage.tsx';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import LogIn from './components/LogIn';
 import { Provider, useSelector } from 'react-redux';
-import { RootState, store } from './store/store.ts';
-// import { SignUp } from './components/SignUp.tsx'; 
+import { RootState, store } from './store/store.ts'; 
 import './css/Header.css'
 import './css/Main.css'
 import './css/Carousal.css'
 import './css/FilterBar.css'
 import './css/Podcast-Preview.css'
 import './css/Footer.css'
-import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
