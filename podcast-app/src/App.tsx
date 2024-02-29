9f6a7ba248f36aa13530776a4e1fe55fae25e6ab
ORATILE  MOKGOATLHENG <125035665+OratileMokgoatlheng@users.noreply.github.com>
2024-02-29 23:20:51 +0200

    refined my code
    removed commented code that is not in use

--- podcast-app/src/App.tsx
+++ podcast-app/src/App.tsx
@@ -1,40 +1,11 @@
-// //import { useState } from 'react'
-// import "./App.css";
-// import LogIn from "./components/LogIn";
-// import Preview from "./components/Previews";
-// import Seasons from "./components/Seasons";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// function App() {
-//   //const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <Router>
-//         <Routes>
-//           <Route path="/" element={<LogIn />} />
-//           <Route path="/preview" element={<Preview />} />
-//           <Route path="/season/:podcastId" element={<Seasons />} />
-//         </Routes>
-//       </Router>
-//     </>
-//   );
-// }
-
-// export default App;
-
 import React, { useState, useEffect } from 'react';
 
 import { Header } from './components/Header'
 import { Carousal } from './components/Carousal'
 import { PodcastPreview } from './components/Podcast-Preview'
-import  Footer  from './components/Footer'
-import {CircularProgress } from '@mui/material';
+import Footer from './components/Footer'
+import { CircularProgress } from '@mui/material';
 
-
-
-
- 
 type AllShowData = Array<ShowPreview>;
 type ShowOriginalData = Array<Show>
 
@@ -82,7 +53,7 @@ export const App: React.FC = () => {
 
 
   useEffect(() => {
-    
+
     const fetchData = async () => {
       try {
         setIsLoading(true);
@@ -93,50 +64,48 @@ export const App: React.FC = () => {
         const json = await response.json() as ShowOriginalData;
         setPodcastData(json)
 
-         // Set isLoading to false after data fetching is complete
-         setIsLoading(false);
+        // Set isLoading to false after data fetching is complete
+        setIsLoading(false);
       }
       catch (error) {
         console.log('Error fetching data:', error);
-         setIsLoading(false);
+        setIsLoading(false);
       }
     };
-    
+
     void fetchData();
   }, []);
 
-    const showId = podcastData.flatMap((show) =>{
-        return(
-          show.id
-        )
-    })
-  
+  const showId = podcastData.flatMap((show) => {
+    return (
+      show.id
+    )
+  })
+
   return (
     <div>
-       {/* Conditional rendering based on isLoading */}
+      {/* Conditional rendering based on isLoading */}
       {isLoading ? (
         <div className="loading__container">
-        <CircularProgress size={40} color="inherit" className="loading__open"/> 
-       </div> 
-        ) : ( 
-       <div>
-        <Header />
-        <br></br>
-        <Carousal 
-         data = {podcastData} 
-        />
-        <br></br>
-        <PodcastPreview
-         data= {podcastData}
-         showIds={showId}
-        // favorites={favorites}
-        // setFavorites={setFavorites}
-        />
-        <Footer/>
-        
-       </div>
-       )
-     }
+          <CircularProgress size={40} color="inherit" className="loading__open" />
+        </div>
+      ) : (
+        <div>
+          <Header />
+          <br></br>
+          <Carousal
+            data={podcastData}
+          />
+          <br></br>
+          <PodcastPreview
+            data={podcastData}
+            showIds={showId}
+          />
+          <Footer />
+
+        </div>
+      )
+      }
     </div>
   )
 }
